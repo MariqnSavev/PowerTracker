039924a95b63015ab13e5c605f697e9a0465d883
eon <dobrin_k1@abv.bg>
2024-10-18 12:06:42 +0300

    Add project files.

---
+++ Data/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TrainigBoxingApp.Models // Уверете се, че имате правилното пространство от имена
+{
+    public class User
+    {
+        public int Id { get; set; } // добавете ID, ако е нужно
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Username { get; internal set; }
+    }
+}
---
+++ Data/Workout.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TrainigBoxingApp.Models
+{
+    public class Workout
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; } // Дата на тренировката
+
+        [Required(ErrorMessage = "Моля, въведете тип на тренировката.")]
+        public string Type { get; set; } = string.Empty; // Тип на тренировката
+
+        [Range(1, 300, ErrorMessage = "Продължителността трябва да бъде между 1 и 300 минути.")]
+        public int Duration { get; set; } // Продължителност в минути
+
+        public string Notes { get; set; } = string.Empty; // Допълнителни бележки
+
+        public string Description { get; set; } = string.Empty; // Описание на тренировката
+
+        [Required(ErrorMessage = "Моля, въведете ID на потребителя.")]
+        public int UserId { get; set; } // ID на потребителя 
+
+        [Required]
+        public User User { get; set; } // Връзка с модела User
+    }
+}
