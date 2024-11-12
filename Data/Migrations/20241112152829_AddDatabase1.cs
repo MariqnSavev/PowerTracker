d72a6580f38f2fe5a7d3ce718a68ac4656896419
MariqnSavev06 <msavev832@gmail.com>
2024-11-12 17:38:47 +0200

    update

---
+++ Data/Migrations/20241112152829_AddDatabase1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BoxingAppDiploma.Data.Migrations
+{
+    public partial class AddDatabase1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Goal",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TargetDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsAchieved = table.Column<bool>(type: "bit", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Goal", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Goal");
+        }
+    }
+}
---
+++ Models/Goal.cs
@@ -0,0 +1,12 @@
+﻿namespace BoxingAppDiploma.Models
+{
+    public class Goal
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime TargetDate { get; set; }
+        public bool IsAchieved { get; set; }
+        public int UserId { get; set; }
+       
+    }
+}
